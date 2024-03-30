9383a283c3e85fc19fd2019ee79ba563fbb2cca6
hojoon0724 <hojoon0724@gmail.com>
2024-03-30 15:55:27 -0700

    added cors

--- opusManifest_backend/settings.py
+++ opusManifest_backend/settings.py
@@ -46,17 +46,21 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "compositions.apps.CompositionsConfig",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "opusManifest_backend.urls"
 
